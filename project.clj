a4fa659ed74673e5f4c23bb9fedbc1efdddb78a1
Sagar Vrajalal <sagar.vrajalal@unifize.com>
2023-09-17 17:23:55 +0530

    Bump version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/clojurescript "1.11.60"]
                  [party.donut/system "0.0.167"]
-                 [com.hyperfiddle/electric "v2-alpha-349-ge9996713"]
+                 [com.hyperfiddle/electric "v2-alpha-428-g22937f75"]
                  [ring-basic-authentication/ring-basic-authentication "1.1.1"]
                  [info.sunng/ring-jetty9-adapter "0.18.5"]
                  [com.taoensso/timbre "6.1.0"]
@@ -15,5 +15,8 @@
   :main ^:skip-aot com.sagar.casa
   :target-path "target/%s"
   :repl-options {:init-ns user}
+  :jvm-opts ["-Xss2m" ; https://github.com/hyperfiddle/photon/issues/11
+             "-XX:-OmitStackTraceInFastThrow" ;; RCF
+             ]
   :aliases {"build" ["run" "-m" "com.sagar.casa/build"]}
   :profiles {:uberjar {:aot :all}})
