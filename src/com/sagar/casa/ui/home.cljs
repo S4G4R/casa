8f467ff747ff5c03ab399982be33f636e213cb58
Sagar Vrajalal <sagar.vrajalal@unifize.com>
2023-11-19 23:35:28 +0530

    Fixup

--- src/com/sagar/casa/ui/home.cljs
+++ src/com/sagar/casa/ui/home.cljs
@@ -1,16 +1,19 @@
 (ns com.sagar.casa.ui.home
-  (:require ["react-bootstrap" :refer [Card Col Container ListGroup Row]]))
+  (:require ["react-bootstrap" :refer [Stack Container Button]]
+            [com.sagar.casa.ui.routes :as routes]))
 
 
 (defn Home []
   [:> Container {:data-theme :light
                  :class-name "p-5"
                  :style {:text-align :center}}
-   [:> Row
-    [:> Col
-     [:h1 {:class-name "display-3"} "Hi. I'm Sagar."]]]
-   [:> Row {:class-name "p-5"}
-    [:> Col
-     [:p "I'm a backend developer."]]
-    #_[:> Col
-     [:h1 {:class-name "display-3"} "Hi. I'm Sagar."]]]])
+   [:> Stack {:gap 2}
+    ;; TODO: Store this stuff in StoryBlok?
+    [:div
+     [:h1 {:class-name "display-4"} "Hi. I'm Sagar."]
+     [:p "I like to write about . . ."]]
+    [:> Stack {:class-name "mx-auto p-5"}
+     [:> Button {:href routes/blog
+                 :variant :dark
+                 :class-name :contrast}
+      "Blog"]]]])
